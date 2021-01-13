eef620d380f23f4952d727c137318d0e827b9228
Seany1990 <seany1990@hotmail.com>
2021-01-13 12:56:58 +0000

    Main Class

---
+++ StringData.java
@@ -0,0 +1,14 @@
+package com.mycompany.stringdata;
+import javax.swing.JFrame;
+
+public class StringData
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new StringDataFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("String Data");
+        frame.setVisible(true);
+    }
+    
+}
